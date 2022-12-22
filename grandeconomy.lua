11d5c341523df64279412adfab0c7b5f79cbaaa5
William <qualitycontentquestionmark@gmail.com>
2022-12-21 23:58:26 -0800

    Create grandeconomy.lua

---
+++ grandeconomy.lua
@@ -0,0 +1,20 @@
+-- for transferring grand economy balances to CCash
+
+local ccash = require("ccash.api")
+ccash.meta.set_server_address("http://54.38.56.148/")
+
+print("enter player to transfer")
+local player_name = read()
+local count, success, result = commands.exec("/wallet balance " .. player_name)
+
+local i = #(success[1]) - 3
+while (string.sub(success[1], i, i) ~= " ") do
+    i = i - 1
+end
+local balance = tonumber(string.sub(success[1], i + 1, #(success[1]) - 2))
+if ccash.admin.impact_bal("admin", "Ys^142BG9%rA", player_name, balance * 100) then 
+    commands.exec("/wallet set " .. player_name .. " 0")
+    print("transfer successful!\n transferred " .. balance .. "gp -> " .. balance * 100 .. "CSH")
+else
+    print("transfer failed")
+end
