e4b4dc389f2c2d095c3614d1829b4d8b5695c173
William <qualitycontentquestionmark@gmail.com>
2022-12-28 20:48:24 -0800

    Update startup.lua

--- backend/startup.lua
+++ backend/startup.lua
@@ -1,28 +1,40 @@
+local ccash = require("ccash.api")
+ccash.meta.set_server_address("url")
+
 rednet.open("back")
 
 while true do
     local id, message = rednet.receive()
     local message = textutils.unserialize(message)
+    local price = 0
+
+    if message[5] == "minecraft:redstone" then
+        price = 3
+    elseif message[5] == "minecraft:coal" then
+        price = 8
+    elseif message[5] == "minecraft:lapis_lazuli" then
+        price = 5
+    elseif message[5] == "minecraft:iron_ingot" then
+        price = 20
+    elseif message[5] == "minecraft:gold_ingot" then
+        price = 50
+    elseif message[5] == "minecraft:emerald" then
+        price = 100
+    elseif message[5] == "minecraft:diamond" then
+        price = 250
+    end
 
     if message[4] then
-        local count, success, _ = commands.exec("/clear " .. message[1] .. " " .. message[5])
-        if count then 
-            local i = #(success[1]) - 6
-            while (string.sub(success[1], i, i) ~= " ") do
-                i = i - 1
-            end
-            local amount_removed = tonumber(string.sub(success[1], i + 1, #(success[1]) - 5))
-            -- notify commoditites exchange of digital inventory change
+        local count, success, amount = commands.exec("/clear " .. message[1] .. " " .. message[5])
+        if count then
+            print(message[1] .. " +" .. price * amount)
+            ccash.impact_bal("admin", "root", message[1], price * amount)
         end
-        rednet.send(id, success[1])                
+        rednet.send(id, success[1])      
     else
-        -- get number of items in digital inventory from commodoties exchange
-        local amount_withdrawn = 184 -- example value of 184
-        while amount_withdrawn >= 64 do
-            commands.exec("/give " .. message[1] .. " " .. message[5] .. " " .. 64)
-            amount_withdrawn = amount_withdrawn - 64
-        end
-        commands.exec("/give " .. message[1] .. " " .. message[5] .. " " .. amount_withdrawn)
+        local amount = math.floor(ccash.get_bal(message[1]) / price)
+        ccash.impact_bal("admin", "root", message[1], -(price * amount))
+        print(message[1] .. " -" .. price * amount)
+        commands.exec("/give " .. message[1] .. " " .. message[5] .. " " .. amount)
     end
 end
-    
