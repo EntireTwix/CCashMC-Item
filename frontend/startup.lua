dc1b5fc29a599db850d60207fc03102db398d5bd
William <qualitycontentquestionmark@gmail.com>
2022-12-28 20:47:55 -0800

    Update startup.lua

--- frontend/startup.lua
+++ frontend/startup.lua
@@ -1,6 +1,7 @@
 local ccash = require("ccash.api")
 ccash.meta.set_server_address("url")
-local backend_id = 2
+
+local backend_id = 1
 
 rednet.open("back")
 
@@ -16,7 +17,7 @@ while true do
     local pass = read("*")
     print()
 
-    if not ccash.verify_password(name, pass) then
+    if not print(ccash.verify_password(name, pass)) then
         print("invalid credentials")    
         os.sleep(2)
     else
@@ -31,6 +32,9 @@ while true do
         local item_id = read()
         if string.find(item_id, " ") then
             print("item_id must not contain spaces")
+            os.sleep(2)
+        elseif item_id ~= "minecraft:redstone" and item_id ~= "minecraft:coal" and item_id ~= "lapis_lazuli" and item_id ~= "minecraft:iron_ingot" and item_id ~= "minecraft:gold_ingot" and item_id ~= "minecraft:emerald" and item_id ~= "minecraft:diamond" then
+            print ("item_id isnt one of the supported ores")
             os.sleep(2)
         else
             rednet.send(backend_id, textutils.serialize({name, pass, answer, deposit, item_id}, {compact = true, allow_repetitions = true}))
